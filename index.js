47f514e01a8241b39076e8342f2df2a0682b6a11
Michael M <mic@hael.xyz>
2022-03-14 13:07:50 -0500

    refactor and add homework function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const colors = require('colors/safe');
+
+/**
+ * Return the name of the school
+ * @return {string}
+ */
+function school() {
+  return 'paintier lowschool';
+}
+
+/**
+ * Return an accurate representation of homework
+ * @return {string}
+ */
+function homework() {
+  return colors.zalgo('homework homework homework homework homework homework');
+}
+
 module.exports = {
-  school: () => {
-    return 'paintrier lowschool';
-  },
+  school: school,
+  homework: homework,
 };
